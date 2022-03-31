536781d130b46e03542bfc1f8c384c0d3e0fb886
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2022-03-31 23:11:56 +0200

    Update changePerfect.js

--- changePerfect.js
+++ changePerfect.js
@@ -1,16 +1,20 @@
 import { _ } from "lodash";
 
-const collection=[{name:"MANI"},{family:"MOVASSAGH"}]
-const secondCollection=[]
-collection.forEach((element,index)=>{
-  const obj={}
-  //console.log(element,index)
-  const entries=Object.keys(element)
-  const values=Object.values(element)
-obj.title=_.startCase(_.toLower(entries));
-obj.values=_.startCase(_.toLower(values));
-  //console.log(obj)
-  secondCollection.push(obj);
-return secondCollection;  
-})
-console.log(secondCollection)
+const collection = [{ name: "MANI" }, { family: "MOVASSAGH" }];
+const secondCollection = [];
+function reviseFetchedData() {
+  collection.forEach((element, index) => {
+    const _obj = {};
+    //console.log(element,index)
+    const entries = Object.keys(element);
+    const values = Object.values(element);
+    _obj.title = _.startCase(_.toLower(entries));
+    _obj.values = _.startCase(_.toLower(values));
+    //console.log(obj)
+    secondCollection.push(_obj);
+  });
+  return secondCollection;
+}
+reviseFetchedData();
+console.log(secondCollection);
+
